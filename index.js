3fcbf03ee890fcfc8dbb702879568325e62dba1c
Muhammad Rendusara <hadyanrendusara@yahoo.com>
2021-11-12 00:25:25 -0600

    commit tree ca87c919b9ae27c34b2ca132329956548888b256

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+const manipulateDom = () => {
+    const p = document.getElementById("text");
+    // p.textContent = "This is really cool!";
+    
+}
+manipulateDom()
+// document.addEventListener("DOMContentLoaded", manipulateDom())
